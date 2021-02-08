6ffa5372520ab482aef59f5d88043fc9c3b6c6ee
Vivek Kumar <vk997620@gmail.com>
2021-02-08 20:46:30 +0530

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-// app.use(express.static('./dist/paysafe-checkout-master'));
+app.use(express.static(__dirname + '/dist/paysafe-checkout-master'));
 
 
 // app.get(`/*`, function(req, res) {
