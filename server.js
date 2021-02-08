08c235965899acf9d94dddae4a6ba902b189c8df
Vivek Kumar <vk997620@gmail.com>
2021-02-08 19:22:02 +0530

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -5,9 +5,15 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/roiim-paysafe'));
+app.use(express.static('./dist/paysafe-checkout-master'));
 
-app.use(express.static("./dist/roiim-paysafe"));
+
+app.get(`/*`, function(req, res) {
+    res.sendFile(`index.html`, {root: 'dist/paysafe-checkout-master/'}
+  );
+  });
+
+// app.use(express.static("./dist/roiim-paysafe"));
 
 
 // app.get('/*', function(req,res) {
@@ -15,10 +21,10 @@ app.use(express.static("./dist/roiim-paysafe"));
 // res.sendFile(path.join(__dirname+'/dist/roiim-paysafe/index.html'));
 // });
 
-app.get('/*', function(req, res) {
-    res.sendFile('index.html', {root: 'dist/roiim-paysafe/'}
-  );
-  });
+// app.get('/*', function(req, res) {
+//     res.sendFile(`C:\\Users\\dell\\Desktop\\vivek_paysafe\\paysafe-checkout-master\\src\\index.html`
+//   );
+//   });
 
 
 // Start the app by listening on the default Heroku port
