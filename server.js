2263df5bda55026e58c96f2fca1ddde00e13cbab
Vivek Kumar <vk997620@gmail.com>
2021-02-08 20:40:34 +0530

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const app = express();
 
 // app.use(express.static(__dirname + '/dist/paysafe-checkout-master'));
 
-app.get('/*', function(req,res) {
+app.get('*', function(req,res) {
     
 res.sendFile(path.join(__dirname+'/dist/paysafe-checkout-master/index.html'));
 });
